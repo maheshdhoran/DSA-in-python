1859a8f90b8a2807013ea6994bb8ca7e51e1d81f
Mahesh Dhoran <maheshdhoran5456@gmail.com>
2020-07-05 15:56:03 +0530

    Iterative and Recursive

--- Algorithms/Searching/Binary_Search_Iteration.py
+++ Algorithms/Searching/Binary_Search_Iteration.py
@@ -1,18 +1,22 @@
 #complexity of code O(logn)
+#Using Iteration
 
 arr=[1,2,3,4,5,55,66,77,78,79,88,89,90,93,99]
 
 def binary(arr,n):
     lowerBound=0
     upperBound=len(arr)-1
-    while l<=u:
+    
+    while lowerBound<=upperBound:
         
         mid=(lowerBound+upperBound)//2
         
         if arr[mid]==n:
             return mid
+            
         elif n<arr[mid]:
             upperBound=mid-1
+            
         elif n>arr[mid]:
             lowerBound=mid+1
     else:
