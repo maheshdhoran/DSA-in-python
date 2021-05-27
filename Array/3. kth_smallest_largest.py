bca4842be9f5fb3976c4524c8f686e822d70a596
Mahesh Dhoran <maheshdhoran5456@gmail.com>
2021-05-27 12:01:55 +0530

    Update 3. kth_smallest_largest.py

--- Array/3. kth_smallest_largest.py
+++ Array/3. kth_smallest_largest.py
@@ -1,10 +1,19 @@
 #1 By Sorting
-# Complexity Time- O(NlogN)
+# Complexity Time- O(nlogn)
 def kth_smallest(arr,k):
     arr.sort()
     return arr[k-1]
 arr=[55,23,1,5,3]
 k=3
 print(kth_smallest(arr,k))
+
+#2 By Sorting
+# Complexity Time- O(nlogn)
+def kth_largest(arr,k):
+    arr.sort(reverse=True)
+    return arr[k-1]
+arr=[55,23,1,5,30]
+k=2
+print(kth_largest(arr,k))
 
 
